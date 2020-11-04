02ae8dd2c500d0a73bc4f2468fd62b9639ab6f2d
Elizabeth Brasseale <eab32@uw.edu>
2020-11-04 10:58:06 -0600

    instead of saving the whole time series, try averaging as you go

--- x_surface_dye.py
+++ x_surface_dye.py
@@ -27,11 +27,17 @@ nfiles = len(f_list)
 
 buoylon = -117.169
 buoylat = 32.570
+shorenormal = 259.03
 
 # Time steps are inconsistent across files, so first count 'em up
 NT = 0
+NT_N = 0
+NT_S = 0
+
 for fn in f_list:
+    
     ds = nc.Dataset(dir0+fn)
+    
     if NT==0:
         nt,nz,ny,nx = ds['salt'].shape
         
@@ -42,17 +48,28 @@ for fn in f_list:
         refgrid = np.abs(lon_rho-buoylon)+np.abs(lat_rho-buoylat)
         buoy_x = np.where(refgrid==refgrid.min())[1][0]
         buoy_y = np.where(refgrid==refgrid.min())[0][0]
+        
+        Dwave0 = ds['Dwave'][:]
+        Dwave = Dwave0[:,buoy_y,buoy_x]
+        nt_N = np.count(Dwave>shorenormal)
+        nt_S = np.count(Dwave<shorenormal)
 
     else:
         nt = ds['ocean_time'].shape[0]
+        Dwave0 = ds['Dwave'][:]
+        Dwave = Dwave0[:,buoy_y,buoy_x]
+        nt_N = np.count(Dwave>shorenormal)
+        nt_S = np.count(Dwave<shorenormal)
+        
     NT += nt
+    NT_N += nt_N
+    NT_S += nt_S
     ds.close()
 
-dye_01 = np.zeros((NT,ny,nx))
-dye_02 = np.zeros((NT,ny,nx))
-Dwave = np.zeros((NT))
-Hwave = np.zeros((NT))
-ot = np.zeros((NT))
+dye_01_N = np.zeros((ny,nx))
+dye_01_S = np.zeros((ny,nx))
+dye_02_N = np.zeros((ny,nx))
+dye_02_S = np.zeros((ny,nx))
 
 # Now do the extraction and processing
 tt=0
@@ -61,28 +78,32 @@ for fn in f_list:
     print('file {:d} of {:d}'.format(tt,nfiles))
     ds = nc.Dataset(dir0+fn)
 
-    # select wave direction and significant wave height
+    # read in dye and wave direction
 
     dye_01_0 = ds['dye_01'][:]
     dye_02_0 = ds['dye_02'][:]
     Dwave0 = ds['Dwave'][:]
-    Hwave0 = ds['Hwave'][:]
-
-    ocean_time = ds['ocean_time'][:]
-    nt = ocean_time.shape[0]
-
-    dye_01[old_nt:old_nt+nt,:,:] = dye_01_0[:,-1,:,:]
-    dye_02[old_nt:old_nt+nt,:,:] = dye_02_0[:,-1,:,:]
+    
+    # only consider wave direction at buoy   
+    Dwave = Dwave0[:,buoy_y,buoy_x]
+    nt = Dwave.shape[0]
+    
+    for t in range(nt):
         
-    Dwave[old_nt:old_nt+nt] = Dwave0[:,buoy_y,buoy_x]
-    Hwave[old_nt:old_nt+nt] = Hwave0[:,buoy_y,buoy_x]
-    ot[old_nt:old_nt+nt] = ocean_time
-    old_nt += nt
+        # if waves from north...
+        if Dwave[t]>shorenormal:
+            dye_01_N += dye_01_0[t,:,:]/NT_N
+            dye_02_N += dye_02_0[t,:,:]/NT_N
+            
+        # if waves from south...
+        elif Dwave[t]<shorenormal:
+            dye_01_S += dye_01_0[t,:,:]/NT_S
+            dye_02_S += dye_02_0[t,:,:]/NT_S 
     
     ds.close()
     tt+=1
 
-var_list = ['dye_01','dye_02','Dwave','Hwave','ot','lon_rho','lat_rho','mask_rho']
+var_list = ['dye_01_N','dye_01_S','dye_02_N','dye_02_S','NT_N','NT_S','NT','lon_rho','lat_rho','mask_rho']
 
 D = dict()
 for var in var_list:
