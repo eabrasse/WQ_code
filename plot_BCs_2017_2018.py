fixed datetime units and abbreviated 2018 times steps so that tidal signals would be visible

plot_BCs_2017_2018.py
@@ -43,10 +43,20 @@ col = 0
 for fn in BC_2017,BC_2018:
     ds = nc.Dataset(fn)
     tt = ds['temp_time'][:]
-
+    
+    if col==0:
+        t0=-1
+        # dt_list = []
+        # for ott in tt:
+        #     dt_list.append(datetime(1999,1,1,0,0) + timedelta(days=ott))
+    
+    if col>0:
+        t0 = len(dt_list)
+        
     dt_list = []
-    for ott in tt:
-        dt_list.append(datetime(1999,1,1,0,0) + timedelta(seconds=ott))
+    for ott in tt[:t0]:
+        dt_list.append(datetime(1999,1,1,0,0) + timedelta(days=ott))
+        
     
     row=0
     for edge in 'south','north','west':
@@ -59,7 +69,7 @@ for fn in BC_2017,BC_2018:
         else:
             ax = fig.add_subplot(gs[row,col],sharex=ax0)
             
-        v = ds[var][:]
+        v = ds[var][:t0,:]
         y = np.arange(0,v.shape[1])
         p = ax.pcolormesh(dt_list,y,np.transpose(v),cmap='rainbow')
         ax.text(0.1,0.9,edge,color='black',fontweight='bold',transform=ax.transAxes)