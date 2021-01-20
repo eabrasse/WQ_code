132fc08ab3fa67a1bbbe58ad3920cdfefa78b2f6
eabrasse <ebrasseale@ucsd.edu>
2021-01-19 18:39:00 -0600

    reduced vertical dimension to allow for easier plotting

--- plot_2018_ROMS_rivers.py
+++ plot_2018_ROMS_rivers.py
@@ -49,26 +49,27 @@ for t in rt:
 # rQ = dsriv['river_transport'][:]
 rT = dsriv['river_temp'][:]
 
+rT = np.mean(rT,axis=1)
 nt,nriv = rT.shape
 
 fig,ax=plt.subplots(4,1,sharex=True,figsize=(8,10))
 
-ax[0].plot(rt_list,-rT[:,:5])
+ax[0].plot(rt_list,rT[:,:5])
 ax[0].text(0.9,0.9,'Tijuana River Estuary (1–5)',transform=ax[0].transAxes,ha='right')
 ax[0].set_ylabel('temp (deg C)')
 ax[0].get_xaxis().set_visible("false")
 
-ax[1].plot(rt_list,-rT[:,5])
+ax[1].plot(rt_list,rT[:,5])
 ax[1].set_ylabel('temp (deg C)')
 ax[1].get_xaxis().set_visible("false")
 ax[1].text(0.9,0.9,'Punta Bandera (6)',transform=ax[1].transAxes,ha='right')
 
-ax[2].plot(rt_list,-rT[:,6:8])
+ax[2].plot(rt_list,rT[:,6:8])
 ax[2].set_ylabel('temp (deg C)')
 ax[2].get_xaxis().set_visible("false")
 ax[2].text(0.9,0.9,'Sweetwater (7–8)',transform=ax[2].transAxes,ha='right')
 
-ax[3].plot(rt_list,-rT[:,8])
+ax[3].plot(rt_list,rT[:,8])
 ax[3].set_ylabel('temp (deg C)')
 ax[3].get_xaxis().set_visible("false")
 ax[3].text(0.9,0.9,'Otay (9)',transform=ax[3].transAxes,ha='right')
