7f086a82be1c07c9d0f6f599eda931a3c055ad0b
eabrasse <ebrasseale@ucsd.edu>
2021-01-26 19:01:45 -0600

    different time, plus added shading = auto to pcolormesh statement to suppress the warnings

--- plot_2018_ROMS_nam.py
+++ plot_2018_ROMS_nam.py
@@ -37,14 +37,14 @@ plt.close('all')
 dir0 = '/data0/ebrasseale/NADB2018/Input/'
 LV4_grid = dir0+'GRID_SDTJRE_LV4_ROTATE_rx020_hplus020_DK_4river_otaymk.nc'
 # LV4_nam_2018 = dir0+'roms_nam_LV4_20171221_20180110.nc'
-LV4_nam_2018 = dir0+'roms_nam_LV4_20180331_20180420.nc'
-# LV4_nam_2018 = dir0+'roms_nam_LV4_20180818_20180907.nc'
+# LV4_nam_2018 = dir0+'roms_nam_LV4_20180331_20180420.nc'
+LV4_nam_2018 = dir0+'roms_nam_LV4_20180818_20180907.nc'
 # LV4_nam_2018 = dir0+'roms_nam_LV4_20181216_20190101.nc'
 
 dir0_nam = '/home/x1wu/SDTJRE_2018/mfiles/NAM_data/LV3/'
 # LV3_nam_2018 = dir0_nam+ 'roms_nam_LV3_20171221_20180120.nc'
-LV3_nam_2018 = dir0_nam+ 'roms_nam_LV3_20180321_20180420.nc'
-# LV3_nam_2018 = dir0_nam+ 'roms_nam_LV3_20180818_20180917.nc'
+# LV3_nam_2018 = dir0_nam+ 'roms_nam_LV3_20180321_20180420.nc'
+LV3_nam_2018 = dir0_nam+ 'roms_nam_LV3_20180818_20180917.nc'
 # LV3_nam_2018 = dir0_nam+ 'roms_nam_LV3_20181216_20190115.nc'
 
 dgrd = nc.Dataset(LV4_grid)
@@ -65,7 +65,7 @@ vwind_LV3 = dlv3['Vwind'][:]
 pair_LV3 = dlv3['Pair'][:]
 
 # now match time indexes between the two grids
-date0 = datetime(2018,4,1)
+date0 = datetime(2018,9,1)
 
 wt3 = dlv3['wind_time'][:]
 wt3_list = []
@@ -89,7 +89,7 @@ gs = GridSpec(3,2)
 vmin = -5
 vmax = 5
 ax0 = fig.add_subplot(gs[0,0])
-p=ax0.pcolormesh(lonr_LV3,latr_LV3,uwind_LV3[t3,:],cmap='BrBG',vmin=vmin,vmax=vmax)
+p=ax0.pcolormesh(lonr_LV3,latr_LV3,uwind_LV3[t3,:],cmap='BrBG',vmin=vmin,vmax=vmax,shading='auto')
 cbaxes = inset_axes(ax0, width="4%", height="40%", loc=4,bbox_transform=ax0.transAxes,bbox_to_anchor=(0.15,0.0,1,1))
 cb = fig.colorbar(p, cax=cbaxes, orientation='vertical')
 cb.ax.set_ylabel('wind (ms-1)',rotation=90,labelpad=10,fontweight='bold')
@@ -101,7 +101,7 @@ ax0.text(0.1,0.1,labeltext,transform=ax0.transAxes,fontweight='bold',va='top')
 
 # plot LV3 v wind
 ax1 = fig.add_subplot(gs[1,0])
-ax1.pcolormesh(lonr_LV3,latr_LV3,vwind_LV3[t3,:],cmap='BrBG',vmin=vmin,vmax=vmax)
+ax1.pcolormesh(lonr_LV3,latr_LV3,vwind_LV3[t3,:],cmap='BrBG',vmin=vmin,vmax=vmax,shading='auto')
 ax1.set_ylabel('latitude')
 ax1.set_xlabel('longitude')
 labeltext= 'LV3 \n'+date0.strftime("%m/%d/%Y") + '\nVwind'
@@ -109,7 +109,7 @@ ax1.text(0.1,0.1,labeltext,transform=ax1.transAxes,fontweight='bold',va='top')
 
 # plot LV3 air pressure
 ax2 = fig.add_subplot(gs[2,0])
-p2=ax2.pcolormesh(lonr_LV3,latr_LV3,pair_LV3[t3,:],cmap='RdPu',vmin=1013,vmax=1021)
+p2=ax2.pcolormesh(lonr_LV3,latr_LV3,pair_LV3[t3,:],cmap='RdPu',vmin=1013,vmax=1021,shading='auto')
 ax2.set_ylabel('latitude')
 ax2.set_xlabel('longitude')
 labeltext= 'LV3 \n'+date0.strftime("%m/%d/%Y") + '\nPair'
@@ -120,7 +120,7 @@ cb.ax.set_ylabel('pressure (Pa)',rotation=90,labelpad=10,fontweight='bold')
 
 # LV4 uwind
 ax3 = fig.add_subplot(gs[0,1])
-ax3.pcolormesh(lonr_LV4,latr_LV4,uwind_LV4[t4,:],cmap='BrBG',vmin=vmin,vmax=vmax)
+ax3.pcolormesh(lonr_LV4,latr_LV4,uwind_LV4[t4,:],cmap='BrBG',vmin=vmin,vmax=vmax,shading='auto')
 ax3.set_ylabel('latitude')
 ax3.set_xlabel('longitude')
 labeltext= 'LV4 \n'+date0.strftime("%m/%d/%Y") + '\nUwind'
@@ -128,7 +128,7 @@ ax3.text(0.1,0.1,labeltext,transform=ax3.transAxes,fontweight='bold',va='top')
 
 # LV4 vwind
 ax4 = fig.add_subplot(gs[1,1])
-ax4.pcolormesh(lonr_LV4,latr_LV4,vwind_LV4[t4,:,:],cmap='BrBG',vmin=vmin,vmax=vmax)
+ax4.pcolormesh(lonr_LV4,latr_LV4,vwind_LV4[t4,:,:],cmap='BrBG',vmin=vmin,vmax=vmax,shading='auto')
 ax4.set_ylabel('latitude')
 ax4.set_xlabel('longitude')
 labeltext= 'LV4 \n'+date0.strftime("%m/%d/%Y") + '\nVwind'
@@ -136,7 +136,7 @@ ax4.text(0.1,0.1,labeltext,transform=ax4.transAxes,fontweight='bold',va='top')
 
 # LV4 air pressure
 ax5 = fig.add_subplot(gs[2,1])
-ax5.pcolormesh(lonr_LV4,latr_LV4,pair_LV4[t4,:],cmap='RdPu',vmin=1013,vmax=1021)
+ax5.pcolormesh(lonr_LV4,latr_LV4,pair_LV4[t4,:],cmap='RdPu',vmin=1013,vmax=1021,shading='auto')
 ax5.set_ylabel('latitude')
 ax5.set_xlabel('longitude')
 labeltext= 'LV4 \n'+date0.strftime("%m/%d/%Y") + '\nPair'
