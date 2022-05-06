d0ebaecd37d6c84c56c80c4a4e8b9bc0c3e4319a
eabrasse <ebrasseale@ucsd.edu>
2022-05-06 12:11:23 -0700

    fixed typo

--- x_coawst_nearshore.py
+++ x_coawst_nearshore.py
@@ -63,6 +63,7 @@ for fn in f_list:
         lonshore = np.zeros((len(jjs)))
         latshore = np.zeros((len(jjs)))
         for j in range(len(jjs)):
+            mask_diff = np.where(np.diff(mask_rho[jjs[j],:]))[0]
             lonshore[j] = lon_rho[jjs[j],iis[j]]
             latshore[j] = lat_rho[jjs[j],iis[j]]
             
