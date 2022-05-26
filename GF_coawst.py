e26a7cf1fa79aad550f5fbb4095677653973ac6f
eabrasse <ebrasseale@ucsd.edu>
2022-05-25 17:58:40 -0700

    new code to godin filter CSIDE data

--- GF_coawst.py
+++ GF_coawst.py
@@ -25,128 +25,107 @@ for my_dir in [dir0,dir1,dir2]:
     f_list0 = [my_dir+x for x in f_list0 if x[:9]=='ocean_his']
     f_list.extend(f_list0)
 
-testing=False
+testing=True
 if testing:
-    f_list = f_list[:3]
+    f_list = f_list[:2]
+    print('TESTING!!!')
 
 nfiles = len(f_list)
-nt_guess = 240*nfiles
-ot = np.zeros((nt_guess))
+ndays_guess = nfiles*10 # not every file has ten days, but most do
 
-# build variable list to filter
+# build new netCDF file
+gf_fn = home+'WQ_data/ocean_daily_gf_NADB2017-2018-2019.nc'
+
+print(f'New netCDF file location: {gf_fn}')
+# get rid of the old version, if it exists
+try:
+    os.remove(gf_fn)
+except OSError:
+    pass # assume error was because the file did not exist
+ds2 = nc.Dataset(gf_fn, 'w', format='NETCDF3_64BIT_OFFSET')
+
+#open first file
 ds = nc.Dataset(f_list[0])
-var_2gf_list = [] #variables to godin filter
-var_ngf_list = [] #variables to not godin filter
-for var in ds.variables.keys():
-    var0 = ds[var]
-    try:
-        if 'ocean_time' in var0.coordinates:
-            var_2gf_list.append(var) # if time-varying, add name to "to godin filter" list
-        else:
-            var_ngf_list.append(var) # if not time-varying, add name to "not godin filter" list
-    except:
-        var_ngf_list.append(var) #variable doesn't have 'coordinates'. save it as "not godin filter"
 
-#
-# tic = time.perf_counter()
-# print('Counting up time steps and reading in surface dye...')
-# count =0
-# for fn in f_list:
-#
-#     printout = f'(working on file {count} of {nfiles})'
-#     print(printout)
-#
-#     ds = nc.Dataset(fn)
-#
-#     if count==0:
-#
-#         nt,nz,ny,nx = ds['salt'].shape
-#
-#         lon_rho = ds['lon_rho'][:]
-#         lat_rho = ds['lat_rho'][:]
-#         mask_rho = ds['mask_rho'][:]
-#
-#         refgrid = np.abs(lon_rho-buoylon)+np.abs(lat_rho-buoylat)
-#         buoy_x = np.where(refgrid==refgrid.min())[1][0]
-#         buoy_y = np.where(refgrid==refgrid.min())[0][0]
-#
-#         surf_dye_01 = {}
-#         surf_dye_02 = {}
-#         for cond in cond_list:
-#             surf_dye_01[cond] = np.zeros((ny,nx))
-#             surf_dye_02[cond] = np.zeros((ny,nx))
-#
-#     else:
-#         nt = ds['ocean_time'].shape[0]
-#
-#     # count time steps for each condition
-#     Dwave0 = ds['Dwave'][:]
-#     Dwave = Dwave0[:,buoy_y,buoy_x]
-#     nt_N = np.sum(Dwave>shorenormal)
-#     nt_S = np.sum(Dwave<shorenormal)
-#
-#     rtt0 = np.argmin(np.abs(rt-ds['ocean_time'][0]))
-#     rtt1 = np.argmin(np.abs(rt-ds['ocean_time'][-1]))+1
-#     nt_NQ = np.sum((Dwave>shorenormal)&(Q[rtt0:rtt1]<-5.0))
-#     nt_SQ = np.sum((Dwave<shorenormal)&(Q[rtt0:rtt1]<-5.0))
-#
-#     ot0 = ds['ocean_time'][:]
-#     ot[NT['total']:NT['total']+nt] = ot0
-#     NT['total'] += nt
-#     NT['N'] += nt_N
-#     NT['S'] += nt_S
-#     NT['NQ'] += nt_NQ
-#     NT['SQ'] += nt_SQ
-#
-#     # add up sea surface
-#     dye_01_0 = ds['dye_01'][:]
-#     dye_02_0 = ds['dye_02'][:]
-#
-#     for t in range(nt):
-#
-#         rtt = np.argmin(np.abs(rt-ot0[t]))
-#         # if waves from north...
-#         if Dwave[t]>shorenormal:
-#             surf_dye_01['N'] += dye_01_0[t,-1,:,:]
-#             surf_dye_02['N'] += dye_02_0[t,-1,:,:]
-#
-#             if Q[rtt]<-5.0:
-#                 surf_dye_02['NQ'] += dye_02_0[t,-1,:,:]
-#
-#         # if waves from south...
-#         elif Dwave[t]<shorenormal:
-#             surf_dye_01['S'] += dye_01_0[t,-1,:,:]
-#             surf_dye_02['S'] += dye_02_0[t,-1,:,:]
-#
-#             if Q[rtt]<-5.0:
-#                 surf_dye_02['SQ'] += dye_02_0[t,-1,:,:]
-#
-#
-#     ds.close()
-#     count+=1
-#
-# print('Counting time steps and reading surface dye complete!')
-# toc = time.perf_counter()
-# count_time = f"Counting time steps took {toc-tic:0.4f} seconds"
-# print(count_time)
-#
-# tic = time.perf_counter()
-# print('Dividing cumulative surface dye by time steps to get mean...')
-# # divide by time steps to get mean
-# for cond in cond_list:
-#     surf_dye_01[cond] = surf_dye_01[cond]/NT[cond]
-#     surf_dye_02[cond] = surf_dye_02[cond]/NT[cond]
-# toc = time.perf_counter()
-# count_time = f"Dividing dye took {toc-tic:0.4f} seconds"
-# print(count_time)
-#
-# ot = ot[ot>0]
-# var_list = ['surf_dye_01','surf_dye_02','NT','Dwave','Q','ot','rt','lon_rho','lat_rho','mask_rho']
-#
-# D = dict()
-# for var in var_list:
-#     D[var]=locals()[var]
-#
-# outfn = home + 'WQ_data/surface_dye_2017-2019_Q-5.p'
-# print(f'Saving to {outfn}')
-# pickle.dump(D,open(outfn,'wb'))
+# Copy dimensions to new netCDF file
+for dname, the_dim in ds.dimensions.items():
+    if 'time' in dname:
+        # for our new file, time will be ndays long
+        ds2.createDimension(dname, ndays_guess)
+    else:
+        ds2.createDimension(dname, len(the_dim) if not the_dim.isunlimited() else None)
+
+#generate variables
+# for all variables that are not time varying, copy them into the new file
+var_2gf_list = []
+for v_name,varin in ds.variables.items():
+    outVar = ds2.createVariable(v_name, varin.datatype, varin.dimensions)
+    try:
+        if 'ocean_time' in varin.coordinates:
+            var_2gf_list.append(v_name) # if time-varying, add name to "to godin filter" list
+            shape = [s for s in varin.shape]
+            shape[0] = ndays_guess #change time index
+    
+            outVar[:] = np.zeros((shape))
+        else:
+            outVar[:] = varin[:]
+    except: # some variables don't have 'coordinates' attribute for some reason, hence the except
+        outVar[:] = varin[:]
+nvars = len(var_2gf_list)
+total_days = 0
+for f in range(nfiles):
+    # if mod(f,5):
+    print(f'Working on file {(f+1):} of {nfiles:}'+'\n'+f_list[f])
+    ds = nc.Dataset(f_list[f])
+    if f>0: #open previous file UNLESS f=0
+        ds0 = nc.Dataset(f_list[f-1])
+    if f<nfiles-1: #open following file UNLESS f = nflies-1
+        ds1 = nc.Dataset(f_list[f+1])
+    nt = ds['ocean_time'][:].shape
+    ndays = int(nt/24)
+    
+    #loop through variables, filtering and saving
+    varcount=0
+    for var_name in var_2gf_list:
+        print(f'   filtering {var_name}...')
+        var = ds[var_name]
+        dim = len(var.shape)
+        
+        #godin filtering uses 35 hourly time steps
+        # to get one value for the first and last days of each file,
+        # we need to include extra hourly time steps from 
+        # the files directly before and after.
+        # For the first and last files, those won't be available
+        if f>0: #don't try to read in preceding file for f=0
+            if dim==1: # if one dimensional
+                var0 = ds0[var_name][-24:]
+            else: # if multidimensional
+                var0 = ds0[var_name][-24:,:]
+            var = np.insert(var,0,var0,axis=0) # add earlier file data to start of array
+        
+        if f<nfiles-1: # don't try to read in following file when f = nfiles-1
+            if dim==1:
+                var1 = ds1[var_name][:24]
+            else:
+                var1 = ds1[var_name][:24,:] 
+            var = np.append(var,var1,axis=0) # add later file data to end of array
+        
+        # pick out daily values
+        
+        if dim==1: # if one dimensional
+            var_gf = wqfun.filt_godin(var)
+            ds2[var][total_days:total_days+ndays] = var_gf[35:-35:24] #trim leading & trailing nans and subsample 1/day
+        else: # if multidimensional
+            # filter
+            var_gf = wqfun.filt_godin_mat(var)
+            ds2[var][total_days:total_days+ndays,:] = var_gf[35:-35:24,:] #trim leading & trailing nans and subsample 1/day
+        varcount+=1
+    # clean up
+    total_days+=ndays
+    if f>0:
+        ds0.close()
+        
+print('Finished!')
+ds.close() # since no new ds1 is loaded for the last day, ds1 should refer to the same file as ds
+ds2.close()
+print()
